e5bc0943334aa5e545fced8e9fd0f5ad9e6ad4c9
Jatus <gianmarco.pettinato@gmail.com>
2018-08-31 11:06:16 +0200

    Switched to a clenear direcotry structure

--- Kalk.java
+++
@@ -1,9 +0,0 @@
-package kalk;
-
-import java.util;
-import kalk.model.Model;
-public class Kalk{
-  public static void main(String[] args) {
-      System.out.println("test");
-  }
-}
--- model/factory/GenericFactory.java
+++
@@ -1,8 +0,0 @@
-package model.factory;
-
-import model.color.Color;
-
-
-public interface GenericFactory{
-  public Color create();
-}
