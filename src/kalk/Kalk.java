97c0973c9ba130e0f06aad793db7e7ade8223073
Jatus <gianmarco.pettinato@gmail.com>
2018-08-31 11:01:44 +0200

    Creating New Branch for Eclipse

---
+++ src/kalk/Kalk.java
@@ -0,0 +1,9 @@
+package kalk;
+
+import java.util;
+import kalk.model.Model;
+public class Kalk{
+  public static void main(String[] args) {
+      System.out.println("test");
+  }
+}
---
+++ src/kalk/model/factory/GenericFactory.java
@@ -0,0 +1,13 @@
+/**
+ * @author Gianmarco Pettinato
+ * 
+ */
+
+package kalk.model.factory;
+
+import kalk.model.color.Color;
+
+
+public interface GenericFactory{
+  public Color create();
+}
