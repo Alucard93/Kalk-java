53609bec89cafe5045f30c95f31dde08285b1b67
Jatus <gianmarco.pettinato@gmail.com>
2018-09-02 11:21:17 +0200

    Revert "Merge pull request #3 from Alucard93/master"
    This reverts commit 39e18052f1762d260111a15b9616cf3c600b8b9f.

--- Model/IllegalColorException.java
+++ Model/IllegalColorException.java
@@ -1,15 +1,11 @@
-package kalk.model;
+
 public class IllegalColorException extends Exception
 {
-  /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-private String what;
+  private String what;
   public IllegalColorException(String what){
     this.what=what;
   }
   public String what(){
-    return what;
+    return what
   }
 }
