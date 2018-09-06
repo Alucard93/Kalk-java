2c6c52f28dd225b3a5bfec14bb3645416a57a0bd
Jatus <gianmarco.pettinato@gmail.com>
2018-09-06 16:57:45 +0200

    Create interface Model

--- src/kalk/model/Model.java
+++ src/kalk/model/Model.java
@@ -1,16 +1,21 @@
 package kalk.model;
-import java.util.Vector;
-public interface Model
-{
 
-  Vector<String> availableOperations();
-  Vector<String> allAvailableTypes();
-  int setLeftType(String type);
-  void setLeftValues(Vector<String> values)throws IllegalColorException;
-  int setRightType(String type);
-  void setRightValues(Vector<String> values)throws IllegalColorException;
-  void setOp(String eOperation);
-  void execute() throws IllegalColorException;
-  Vector<String> getResult();
-  Vector<String> getHistory();
+import java.util.Vector;
+
+public interface Model extends Cloneable {
+	public Vector<String> allAvailableTypes();
+	public Vector<String> availableOperations();
+	public void execute() throws IllegalColorException, CloneNotSupportedException;
+	public Vector<String> getHistory();
+	public Vector<String> getResult();	
+	public Vector<String> permittedOperations();
+	public Vector<String> permittedTypes();
+	public int setLeftType(String type);
+	public void setLeftValues(Vector<String> values) throws IllegalColorException;	
+	public void setOp(String operation);	
+	public int setRightType(String type);
+	public void setRightValues(Vector<String> values) throws IllegalColorException; 
+	
+	@Override
+	public String toString();
 }
